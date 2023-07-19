3b90dd2acea19b8564c36c0358cb3cfc283c45e7
westy12dan <westy12dan@gmail.com>
2023-07-19 18:17:58 +0800

    v

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { DataConnection, Peer, PeerJSOption } from 'npm:peerjs@1.4.8-rc.1'
+import { DataConnection, Peer, PeerJSOption } from 'npm:peerjs@1.4.5'
 import initSqlJs from 'npm:sql.js@1.8.0'
 
 export type InitOptions = {
